38af1c2494ab5c9ed2f405df35cce1e24441493b
Mike Beck <mike@mikeybeck.com>
2020-01-07 15:40:24 +1300

    Remove unnecessary code

--- popup.js
+++ popup.js
@@ -26,20 +26,6 @@ var schema = {
 },
 db = new ydn.db.Storage('db-name', schema);
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
-
 checkForNewPrs.onclick = function(element) {
   tryGetPrJson();
 };
